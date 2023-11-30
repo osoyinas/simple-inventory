982c8bc4acadfe86e0fb4fd7cd9b1b7f91dbd521
Pablo Piñas <osoyinas@gmail.com>
2023-11-30 18:15:00 +0100

    Add no hay personas registradas

--- src/renderer/Components/PersonTable.tsx
+++ src/renderer/Components/PersonTable.tsx
@@ -2,33 +2,38 @@ import { Person } from "@/types/api";
 
 export function PersonsTable({ persons }: { persons: Person[] }) {
   return (
-    <table className="table bg-white h-[300px] max-w-6xl min-w-max">
-      <thead className="text-lg font-bold text-black opacity-70">
-        <tr>
-          <th>
-            <label>
-              <input type="checkbox" className="checkbox" />
-            </label>
-          </th>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th>Materiales que ha obtenido</th>
-          <th>Obras en las que trabaja</th>
-        </tr>
-      </thead>
-      <tbody className="text-xl">
-        {persons.map((person) => (
-          <tr key={person.id}>
-            <td>
+    <>
+      <table className="table bg-white h-auto max-w-6xl min-w-max">
+        <thead className="text-lg font-bold text-black opacity-70">
+          <tr>
+            <th>
               <label>
                 <input type="checkbox" className="checkbox" />
               </label>
-            </td>
-            <td>{person.id}</td>
-            <td>{person.name}</td>
+            </th>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Materiales que ha obtenido</th>
+            <th>Obras en las que trabaja</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="text-xl">
+          {persons.map((person) => (
+            <tr key={person.id}>
+              <td>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </td>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {persons.length === 0 && (
+        <p className="text-center">No hay personas registradas</p>
+      )}
+    </>
   );
 }
