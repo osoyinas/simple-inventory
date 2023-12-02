de148a7d25927b02e1771caa73fee94254abf64f
Pablo Piñas <osoyinas@gmail.com>
2023-12-02 22:09:06 +0100

    Add materialView

--- inic.sql
+++ inic.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS Material (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255) NOT NULL,
   units VARCHAR(255) NOT NULL,
-  available_amount INT NOT NULL,
   absolute_amount INT NOT NULL
 );
 
@@ -27,7 +26,20 @@ CREATE TABLE IF NOT EXISTS Movement (
   amount INT,
   date DATE,
   type VARCHAR(3) CHECK (type IN ('IN', 'OUT')) NOT NULL,
-  FOREIGN KEY (id_person) REFERENCES Person(id) ON DELETE SET NULL,
-  FOREIGN KEY (id_material) REFERENCES Material(id) ON DELETE SET NULL,
- FOREIGN KEY (id_work) REFERENCES Work(id) ON DELETE SET NULL
+  FOREIGN KEY (id_person) REFERENCES Person(id),
+  FOREIGN KEY (id_material) REFERENCES Material(id),
+  FOREIGN KEY (id_work) REFERENCES Work(id)
 );
+
+CREATE VIEW MaterialView AS
+SELECT 
+    m.id,
+    m.name,
+    m.units,
+    m.absolute_amount + COALESCE(SUM(CASE WHEN mov.type = 'IN' THEN mov.amount ELSE -mov.amount END), 0) AS available_amount
+FROM 
+    Material m
+LEFT JOIN 
+    Movement mov ON m.id = mov.id_material
+GROUP BY 
+    m.id, m.name, m.units, m.absolute_amount;
