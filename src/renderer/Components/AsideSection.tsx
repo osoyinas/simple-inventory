f30b023e2b197f9f3f69fa18f9717a4eb3ddd72e
Pablo Piñas <osoyinas@gmail.com>
2023-12-05 22:39:36 +0100

    Remove old asidesection

--- src/renderer/Components/AsideSection.tsx
+++
@@ -1,7 +0,0 @@
-export function AsideSection({ children }: { children: React.ReactNode }) {
-    return (
-        <section className="p-12 w-full w-max-[1300px] m-auto relative bg-base-200 text-base-content flex flex-col items-center gap-6">
-            {children}
-        </section>
-    );
-}
