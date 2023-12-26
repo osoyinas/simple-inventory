f5db537df18a0db075af3a8ee995514c8fe03732
Pablo Piñas <osoyinas@gmail.com>
2023-12-26 15:28:25 +0100

    Add sort direction

--- src/renderer/hooks/useSort.tsx
+++ src/renderer/hooks/useSort.tsx
@@ -2,25 +2,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useMemo } from 'react';
 
-
 export function useSort<T>( items: T[] ) {
     const [sort, setSort] = useState<keyof T | null>();
+    const [sortDirection, setSortDirection] = useState<boolean>(true); // true = asc, false = desc
 
+
+    const handleSort = (sort: keyof T | null, sortDirection: boolean) => {
+        setSort(sort)
+        setSortDirection(sortDirection)
+    }
 
     const sortedItems =  useMemo(() => {
-        if (!sort) {
+        if (!sort) 
             return items
-        }
-        return [...items].sort((a: T, b: T) => {
-            if (typeof a[sort as keyof T] === "number") 
-                return (b[sort as keyof T] as number) - (a[sort as keyof T] as number);
-            else if (typeof a[sort as keyof T] === "string") 
-                return (a[sort as keyof T] as string).localeCompare(b[sort as keyof T] as string)
-            else return 0;
-        });
+        
+        if (sortDirection) {
+            return [...items].sort((a: T, b: T) => {
+
+                if (typeof a[sort as keyof T] === "number") 
+                    return (a[sort as keyof T] as number) - (b[sort as keyof T] as number);
+                else if (typeof a[sort as keyof T] === "string") 
+                    return (a[sort as keyof T] as string).localeCompare(b[sort as keyof T] as string)
+                else return 0;
+            });
+        } else
+            return [...items].sort((a: T, b: T) => {
+                if (typeof a[sort as keyof T] === "number") 
+                    return (b[sort as keyof T] as number) - (a[sort as keyof T] as number);
+                else if (typeof a[sort as keyof T] === "string") 
+                    return (b[sort as keyof T] as string).localeCompare(a[sort as keyof T] as string)
+                else return 0;
+            });
 
     }, [sort, items]);
 
 
-    return { sortedItems, setSort };
+    return { sortedItems, handleSort, sortDirection };
 }
