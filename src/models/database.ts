a1eccf78683ba973b3c7c2b26d3e08765a57fb5f
Pablo Piñas <osoyinas@gmail.com>
2023-12-28 20:22:42 +0100

    Change database folder

--- src/models/database.ts
+++ src/models/database.ts
@@ -1,13 +1,13 @@
 import sqlite3 = require("sqlite3");
 import fs = require('fs');
 
-const db = new sqlite3.Database("src/models/database.db", (err: Error | null) => {
+const db = new sqlite3.Database("data/database.db", (err: Error | null) => {
     if (err) {
         console.error(err.message);
     } else {
         console.log("Connected to the SQLite database.");
         // Ejecuta el archivo inic.sql al inicio
-        const sqlFile = fs.readFileSync("src/models/inic.sql", 'utf8');
+        const sqlFile = fs.readFileSync("data/inic.sql", 'utf8');
         db.exec(sqlFile, (err: Error | null) => {
             if (err) {
                 console.error(err.message);
@@ -16,7 +16,7 @@ const db = new sqlite3.Database("src/models/database.db", (err: Error | null) =>
             }
         });
 
-        // const sqlDataFile = fs.readFileSync("src/models/data.sql", 'utf8');
+        // const sqlDataFile = fs.readFileSync("data/data.sql", 'utf8');
         // db.exec(sqlDataFile, (err: Error | null) => {
         //     if (err) {
         //         console.error(err.message);
