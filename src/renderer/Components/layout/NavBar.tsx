e554fb19cc229b946868a2564e420eebed18532e
Pablo Piñas <osoyinas@gmail.com>
2023-12-29 12:55:37 +0100

    Add sticky to navbar

--- src/renderer/Components/layout/NavBar.tsx
+++ src/renderer/Components/layout/NavBar.tsx
@@ -8,7 +8,7 @@ export function Navbar() {
         { name: "Personas", path: "/persons", actual:useMatch("/persons") },
     ];
     return (
-        <nav className="navbar bg-base-100 px-40 py-0 items-center justify-between fixed h-[65px] w-full border z-50 border-b-slate-500 border-r-0">
+        <nav className="sticky top-0 navbar bg-base-100 min-w-[1000px] px-40 py-0 items-center justify-between h-[65px] border z-50 border-b-slate-500 border-r-0">
             <ul className="flex justify-start gap-4 relative h-full">
                 {options.map((option) => {
                     return (
