e57eb3c0f88b129aba7788faa4a8b549a58f1e09
Pablo Piñas <osoyinas@gmail.com>
2023-12-29 12:55:26 +0100

    Remove padding

--- src/renderer/Components/layout/LayoutContainer.tsx
+++ src/renderer/Components/layout/LayoutContainer.tsx
@@ -1,6 +1,6 @@
 export function LayoutContainer({ children }: { children: React.ReactNode }) {
     return (
-        <section className="p-12 w-full w-max-[1300px] m-auto relative bg-base-200 text-base-content flex flex-col items-center gap-6">
+        <section className="w-full w-max-[1300px] m-auto relative bg-base-200 text-base-content flex flex-col items-center gap-6">
             {children}
         </section>
     );
