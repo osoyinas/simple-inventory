90b474467d94880c648853e239c632b090bdd447
Pablo Piñas <osoyinas@gmail.com>
2024-01-27 12:54:46 +0100

    Format with prettier

--- electron/preload.ts
+++ electron/preload.ts
@@ -6,20 +6,20 @@ contextBridge.exposeInMainWorld('ipcRenderer', withPrototype(ipcRenderer))
 
 // `exposeInMainWorld` can't detect attributes and methods of `prototype`, manually patching it.
 function withPrototype(obj: Record<string, any>) {
-  const protos = Object.getPrototypeOf(obj)
+    const protos = Object.getPrototypeOf(obj)
 
-  for (const [key, value] of Object.entries(protos)) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) continue
+    for (const [key, value] of Object.entries(protos)) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) continue
 
-    if (typeof value === 'function') {
-      // Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
-      obj[key] = function (...args: any) {
-        return value.call(obj, ...args)
-      }
-    } else {
-      obj[key] = value
+        if (typeof value === 'function') {
+            // Some native APIs, like `NodeJS.EventEmitter['on']`, don't work in the Renderer process. Wrapping them into a function.
+            obj[key] = function (...args: any) {
+                return value.call(obj, ...args)
+            }
+        } else {
+            obj[key] = value
+        }
     }
-  }
-  return obj
+    return obj
 }
 
