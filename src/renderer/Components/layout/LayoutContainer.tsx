a7275cb94a7354fcc7e81cd11ea677170ff6a06b
Pablo Piñas <osoyinas@gmail.com>
2024-01-27 18:55:45 +0100

    Add classname to layout

--- src/renderer/Components/layout/LayoutContainer.tsx
+++ src/renderer/Components/layout/LayoutContainer.tsx
@@ -1,6 +1,6 @@
-export function LayoutContainer({ children }: { children: React.ReactNode }) {
+export function LayoutContainer({ children, className }: { children: React.ReactNode, className?: string}) {
     return (
-        <section className="w-full w-max-[1300px] m-auto relative bg-base-200 text-base-content flex flex-col items-center gap-6">
+        <section className={`${"w-full w-max-[1300px] m-auto relative bg-base-200 text-base-content flex flex-col items-center gap-6"} ${className}`}>
             {children}
         </section>
     );
